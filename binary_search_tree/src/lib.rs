b253e75afb0d3c492a7c9ab16ff5ee6ada2aaab4
mikong <4162+mikong@users.noreply.github.com>
2019-03-22 23:51:37 +0800

    feat: Implement initial BST with insert

--- binary_search_tree/src/lib.rs
+++ binary_search_tree/src/lib.rs
@@ -1,9 +1,12 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
 #[derive(Debug)]
 pub struct Node {
     key: usize,
     value: String,
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
+    left: Option<Rc<RefCell<Node>>>,
+    right: Option<Rc<RefCell<Node>>>,
 }
 
 impl Node {
@@ -17,14 +20,80 @@ impl Node {
     }
 }
 
+#[derive(Debug)]
+pub struct BST {
+    root: Option<Rc<RefCell<Node>>>,
+}
+
+impl BST {
+    pub fn new() -> Self {
+        BST { root: None }
+    }
+
+    pub fn put(&mut self, key: usize, value: String) {
+        let node = BST::insert(&self.root, key, value);
+        self.root = Some(node);
+    }
+
+    fn insert(x: &Option<Rc<RefCell<Node>>>, key: usize, value: String) -> Rc<RefCell<Node>> {
+        if let Some(node) = x {
+            if key < node.borrow().key {
+                let new_node = BST::insert(&node.borrow().left, key, value);
+                node.borrow_mut().left = Some(new_node);
+            } else if key > node.borrow().key {
+                let new_node = BST::insert(&node.borrow().right, key, value);
+                node.borrow_mut().right = Some(new_node);
+            }
+            // TODO: if same key, update value
+            return Rc::clone(node);
+        }
+        // x = None
+        Rc::new(RefCell::new(Node::new(key, value)))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn check_key(cell: &Option<Rc<RefCell<Node>>>, key: usize) {
+        if let Some(node) = cell {
+            assert_eq!(node.borrow().key, key);
+        } else {
+            panic!("Node can't be None");
+        }
+    }
+
     #[test]
-    fn it_works() {
+    fn create_node() {
         let n = Node::new(1, "a".to_string());
         assert_eq!(n.left.is_none(), true);
         assert_eq!(n.right.is_none(), true);
+    }
+
+    #[test]
+    fn build_tree() {
+        let mut bst = BST::new();
+        assert_eq!(bst.root.is_none(), true);
+
+        // New node becomes root
+        bst.put(2, "b".to_string());
+        check_key(&bst.root, 2);
+
+        // New node becomes left node
+        bst.put(1, "a".to_string());
+        if let Some(node) = &bst.root {
+            check_key(&node.borrow().left, 1);
+        } else {
+            panic!("BST must have root");
+        }
+
+        // New node becomes right node
+        bst.put(3, "c".to_string());
+        if let Some(node) = &bst.root {
+            check_key(&node.borrow().right, 3);
+        } else {
+            panic!("BST must have root");
+        }
     }
 }
