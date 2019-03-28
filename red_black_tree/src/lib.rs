f8c5b99c565092fc431650f06d7e55282be99df1
mikong <4162+mikong@users.noreply.github.com>
2019-03-28 14:19:14 +0800

    feat: Initial Node struct for RBT

--- red_black_tree/src/lib.rs
+++ red_black_tree/src/lib.rs
@@ -1,7 +1,38 @@
+#[derive(Debug)]
+pub enum Color {
+    Red,
+    Black,
+}
+
+#[derive(Debug)]
+pub struct Node<K, V> {
+    key: K,
+    value: V,
+    left: Option<Box<Node<K, V>>>,
+    right: Option<Box<Node<K, V>>>,
+    color: Color,
+}
+
+impl<K, V> Node<K, V> {
+    pub fn new(key: K, value: V, color: Color) -> Self {
+        Node {
+            key,
+            value,
+            left: None,
+            right: None,
+            color,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let n = Node::new(1, "a".to_string(), Color::Red);
+        assert_eq!(n.left.is_none(), true);
+        assert_eq!(n.right.is_none(), true);
     }
 }
