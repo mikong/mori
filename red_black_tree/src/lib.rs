922c93a3a88671ac91d4254a3af23fd76e19fff3
mikong <4162+mikong@users.noreply.github.com>
2019-03-29 15:43:19 +0800

    feat: Implement RBT put with rotation and color flips

--- red_black_tree/src/lib.rs
+++ red_black_tree/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Color {
     Red,
     Black,
@@ -49,6 +49,14 @@ impl<K, V> RedBlackTree<K, V>
         }
     }
 
+    fn new_node(&mut self, key: K, value: V, color: Color) -> NodeId {
+        let next_index = self.nodes.len();
+
+        self.nodes.push(Node::new(key, value, color));
+
+        next_index
+    }
+
     pub fn get(&self, key: K) -> Option<&V> {
         let mut x = &self.root;
         while let Some(node_id) = x {
@@ -64,10 +72,54 @@ impl<K, V> RedBlackTree<K, V>
         None
     }
 
+    pub fn put(&mut self, key: K, value: V) {
+        self.root = self.rput(self.root, key, value);
+
+        if let Some(root_id) = self.root {
+            self.nodes[root_id].color = Color::Black;
+        }
+    }
+
+    fn rput(&mut self, node: Option<NodeId>, key: K, value: V) -> Option<NodeId> {
+        if let Some(node_id) = node {
+            if key < self.nodes[node_id].key {
+                self.nodes[node_id].left = self.rput(self.nodes[node_id].left, key, value);
+            } else if key > self.nodes[node_id].key {
+                self.nodes[node_id].right = self.rput(self.nodes[node_id].right, key, value);
+            } else {
+                self.nodes[node_id].value = value;
+            }
+
+            // Fix any right-leaning links
+            let mut node_id = node_id;
+            let right_is_red = self.is_red(self.nodes[node_id].right);
+            let left_is_red = self.is_red(self.nodes[node_id].left);
+            if right_is_red && !left_is_red {
+                node_id = self.rotate_left(node_id);
+            }
+            if self.is_red(self.nodes[node_id].left) {
+                let left_id = self.nodes[node_id].left.unwrap();
+                if self.is_red(self.nodes[left_id].left) {
+                    node_id = self.rotate_right(node_id);
+                }
+            }
+            let left_is_red = self.is_red(self.nodes[node_id].left);
+            let right_is_red = self.is_red(self.nodes[node_id].right);
+            if left_is_red && right_is_red {
+                self.flip_colors(node_id);
+            }
+
+            return Some(node_id);
+        }
+
+        let node_id = self.new_node(key, value, Color::Red);
+        Some(node_id)
+    }
+
     // Helper methods
 
-    fn rotate_left(&mut self, parent: &Option<NodeId>) -> Option<NodeId> {
-        let old = parent.unwrap();
+    fn rotate_left(&mut self, parent: NodeId) -> NodeId {
+        let old = parent;
         let new = self.nodes[old].right.unwrap();
 
         self.nodes[old].right = self.nodes[new].left;
@@ -75,11 +127,11 @@ impl<K, V> RedBlackTree<K, V>
         self.nodes[new].color = self.nodes[old].color;
         self.nodes[old].color = Color::Red;
 
-        Some(new)
+        new
     }
 
-    fn rotate_right(&mut self, parent: &Option<NodeId>) -> Option<NodeId> {
-        let old = parent.unwrap();
+    fn rotate_right(&mut self, parent: NodeId) -> NodeId {
+        let old = parent;
         let new = self.nodes[old].left.unwrap();
 
         self.nodes[old].left = self.nodes[new].right;
@@ -87,11 +139,10 @@ impl<K, V> RedBlackTree<K, V>
         self.nodes[new].color = self.nodes[old].color;
         self.nodes[old].color = Color::Red;
 
-        Some(new)
+        new
     }
 
-    fn flip_colors(&mut self, node: &Option<NodeId>) {
-        let node = node.unwrap();
+    fn flip_colors(&mut self, node: NodeId) {
         let left = self.nodes[node].left.unwrap();
         let right = self.nodes[node].right.unwrap();
         match self.nodes[node].color {
@@ -106,6 +157,13 @@ impl<K, V> RedBlackTree<K, V>
                 self.nodes[right].color = Color::Black;
             },
         };
+    }
+
+    fn is_red(&self, node: Option<NodeId>) -> bool {
+        match node {
+            Some(node) => self.nodes[node].color == Color::Red,
+            None => false,
+        }
     }
 }
 
@@ -128,5 +186,28 @@ mod tests {
         assert_eq!(tree.get(8), None);
 
         // TODO: non-empty case
+    }
+
+    #[test]
+    fn put() {
+        let mut tree = RedBlackTree::new();
+        tree.put("A".to_string(), 8);
+        tree.put("C".to_string(), 4);
+        tree.put("E".to_string(), 12);
+
+        let root_id = tree.root.unwrap();
+        let root = &tree.nodes[root_id];
+        assert_eq!(root.key, "C".to_string());
+        assert_eq!(root.color, Color::Black);
+
+        let left_id = root.left.unwrap();
+        let left = &tree.nodes[left_id];
+        assert_eq!(left.key, "A".to_string());
+        assert_eq!(left.color, Color::Black);
+
+        let right_id = root.right.unwrap();
+        let right = &tree.nodes[right_id];
+        assert_eq!(right.key, "E".to_string());
+        assert_eq!(right.color, Color::Black);
     }
 }
