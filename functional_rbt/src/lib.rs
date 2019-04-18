d5daae59f50ad04d0631007cd7b4e4cc9495f5e0
mikong <4162+mikong@users.noreply.github.com>
2019-04-19 04:15:05 +0800

    feat: Initial Tree enum, Node struct for Functional RBT

--- functional_rbt/src/lib.rs
+++ functional_rbt/src/lib.rs
@@ -1,7 +1,36 @@
+#[derive(Debug)]
+pub enum Color {
+    Red,
+    Black,
+}
+
+#[derive(Debug)]
+pub enum Tree<T> {
+    Empty,
+    NonEmpty(Box<Node<T>>),
+}
+
+#[derive(Debug)]
+pub struct Node<T> {
+    color: Color,
+    element: T,
+    left: Tree<T>,
+    right: Tree<T>,
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use super::Color::*;
+    use super::Tree::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let _tree = NonEmpty(Box::new(Node {
+            color: Red,
+            element: 5,
+            left: Empty,
+            right: Empty,
+        }));
     }
 }
