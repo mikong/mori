2e1599720bda64cfc1eee30b7073032cadb0ffdd
mikong <4162+mikong@users.noreply.github.com>
2019-04-18 14:59:40 +0800

    feat: Initial Node struct for Heap

--- leftist_heap/src/lib.rs
+++ leftist_heap/src/lib.rs
@@ -1,7 +1,29 @@
+#[derive(Debug)]
+enum Heap<T> {
+    Empty,
+    NonEmpty(Box<Node<T>>),
+}
+
+#[derive(Debug)]
+struct Node<T> {
+    rank: usize,
+    element: T,
+    left: Heap<T>,
+    right: Heap<T>,
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use super::Heap::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let _heap = NonEmpty(Box::new(Node {
+            rank: 0,
+            element: 5,
+            left: Empty,
+            right: Empty,
+        }));
     }
 }
