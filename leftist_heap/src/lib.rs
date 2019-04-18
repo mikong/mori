925c3f3ed752378220f4c9b0e98d2fcec7d64c6f
mikong <4162+mikong@users.noreply.github.com>
2019-04-18 15:31:29 +0800

    Implement Heap rank

--- leftist_heap/src/lib.rs
+++ leftist_heap/src/lib.rs
@@ -1,15 +1,24 @@
 #[derive(Debug)]
-enum Heap<T> {
+pub enum Heap<T> {
     Empty,
     NonEmpty(Box<Node<T>>),
 }
 
 #[derive(Debug)]
-struct Node<T> {
+pub struct Node<T> {
     rank: usize,
     element: T,
     left: Heap<T>,
     right: Heap<T>,
+}
+
+impl<T> Heap<T> {
+    fn rank(&self) -> usize {
+        match self {
+            Heap::Empty => 0,
+            Heap::NonEmpty(ref node) => node.rank,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -19,11 +28,13 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let _heap = NonEmpty(Box::new(Node {
+        let heap = NonEmpty(Box::new(Node {
             rank: 0,
             element: 5,
             left: Empty,
             right: Empty,
         }));
+
+        assert_eq!(heap.rank(), 0);
     }
 }
