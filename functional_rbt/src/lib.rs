88d5958fdcbab83617665fef643c60f3dfaac303
mikong <4162+mikong@users.noreply.github.com>
2019-04-19 16:16:32 +0800

    feat: Implement Functional RBT member

--- functional_rbt/src/lib.rs
+++ functional_rbt/src/lib.rs
@@ -18,6 +18,23 @@ pub struct Node<T> {
     right: Tree<T>,
 }
 
+impl<T: Ord> Tree<T> {
+    pub fn member(&self, element: &T) -> bool {
+        match self {
+            Tree::Empty => false,
+            Tree::NonEmpty(ref node) => {
+                if *element < node.element {
+                    node.left.member(element)
+                } else if *element > node.element {
+                    node.right.member(element)
+                } else {
+                    true
+                }
+            },
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,11 +43,14 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let _tree = NonEmpty(Box::new(Node {
+        let tree = NonEmpty(Box::new(Node {
             color: Red,
             element: 5,
             left: Empty,
             right: Empty,
         }));
+
+        assert_eq!(tree.member(&5), true);
+        assert_eq!(tree.member(&6), false);
     }
 }
