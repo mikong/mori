58f4629427d34fcc266330bc47b6d6a66265c9f3
mikong <4162+mikong@users.noreply.github.com>
2019-04-30 22:25:41 +0800

    feat: Initial Tree enum, Node struct for Splay Heap

--- splay_heap/src/lib.rs
+++ splay_heap/src/lib.rs
@@ -1,7 +1,27 @@
+#[derive(Debug)]
+pub enum Tree<T> {
+    Empty,
+    NonEmpty(Box<Node<T>>),
+}
+
+#[derive(Debug)]
+pub struct Node<T> {
+    element: T,
+    left: Tree<T>,
+    right: Tree<T>,
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use super::Tree::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let _tree = NonEmpty(Box::new(Node {
+            element: 5,
+            left: Empty,
+            right: Empty,
+        }));
     }
 }
