41bfc5d66edf643dfef600b477f2e2da4c980f04
mikong <4162+mikong@users.noreply.github.com>
2019-05-05 13:27:18 +0800

    feat: Initial Heap enum, Node struct for Pairing Heap

--- pairing_heap/src/lib.rs
+++ pairing_heap/src/lib.rs
@@ -1,7 +1,25 @@
+#[derive(Debug)]
+pub enum Heap<T> {
+    Empty,
+    NonEmpty(Box<Node<T>>),
+}
+
+#[derive(Debug)]
+pub struct Node<T> {
+    element: T,
+    list: Vec<Heap<T>>,
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use super::Heap::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let _heap = NonEmpty(Box::new(Node {
+            element: 5,
+            list: vec![],
+        }));
     }
 }
