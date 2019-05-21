91613dcb2761bf0cb3453c776cc3affef9ac69f3
mikong <4162+mikong@users.noreply.github.com>
2019-05-22 00:20:21 +0800

    feat: Initial MerkleTree enum, Node struct

--- merkle_tree/src/lib.rs
+++ merkle_tree/src/lib.rs
@@ -1,7 +1,27 @@
+#[derive(Debug)]
+pub enum MerkleTree<T> {
+    Empty,
+    NonEmpty(Box<Node<T>>),
+}
+
+#[derive(Debug)]
+pub struct Node<T> {
+    element: T,
+    left: MerkleTree<T>,
+    right: MerkleTree<T>,
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use super::MerkleTree::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let _tree = NonEmpty(Box::new(Node {
+            element: 5,
+            left: Empty,
+            right: Empty,
+        }));
     }
 }
